c6dcb1cdcb8d13f0e227bbd8da3dfcdeefb36934
Aleksei Chernov <kot.lex@gmail.com>
2020-05-27 19:21:52 +0300

    Use original cwd

--- iconoteka.config.default.js
+++ iconoteka.config.default.js
@@ -1,12 +1,13 @@
 const path = require('path');
-
+const cwd = process.env.ICONOTEKA_ORIGINAL_CWD || process.cwd();
 const config = {
+    cwd,
     componentsOverrides: 
         {
             
         },
-    iconotekaJson: path.join(process.cwd(), 'iconoteka', 'iconoteka.json'),
-    iconotekaFilesPath: path.join(process.cwd(), 'iconoteka')
+    iconotekaJson: path.join(cwd, 'iconoteka', 'iconoteka.json'),
+    iconotekaFilesPath: path.join(cwd, 'iconoteka')
 };
 
 module.exports = config;
